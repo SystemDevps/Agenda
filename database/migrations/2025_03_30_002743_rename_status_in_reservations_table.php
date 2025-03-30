78ac40b8809155039714d204be8d55a57c70db28
SystemDevps <systemeveloperscompany@gmail.com>
2025-03-29 19:28:35 -0600

    creation migrations

---
+++ database/migrations/2025_03_30_002743_rename_status_in_reservations_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Migrations\Migration;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("ALTER TABLE reservations CHANGE status reservation_status ENUM('pendiente', 'confirmada', 'cancelada') NOT NULL DEFAULT 'pendiente'");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+
+        DB::statement("ALTER TABLE reservations CHANGE reservation_status status ENUM('pendiente', 'confirmada', 'cancelada') NOT NULL DEFAULT 'pendiente'");
+    }
+};
